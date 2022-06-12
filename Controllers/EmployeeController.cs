a9825a7573432f964d7f19f293c57867b1730f39
danish703 <dipu.danish@outlook.com>
2022-06-12 21:29:03 +0545

    initial

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelBindingDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee e)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();           
+        }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ModelBindingDemo.Models
+{
+    public class Employee
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(15)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(10,30)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        [Required]
+        [Compare("password")]
+        public string confirmPassword { get; set; }
+
+        [Required]
+        [Url]
+        public string url { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,Female
+    }
+}
